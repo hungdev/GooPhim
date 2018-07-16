99cb8c0ad3179a4057db918f00cac07a43b9940a
hungdev <hungns126@gmail.com>
2018-07-16 22:54:30 +0700

    config react-native-orientation

--- App.js
+++ App.js
@@ -10,6 +10,7 @@ import React, { Component } from 'react';
 import { Platform, StyleSheet, Text, View } from 'react-native';
 import Icon from 'react-native-vector-icons/Ionicons'
 import VideoPlayer from 'react-native-video-controls'
+import Orientation from 'react-native-orientation'
 
 const instructions = Platform.select({
   ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
@@ -20,12 +21,34 @@ const instructions = Platform.select({
 
 type Props = {};
 export default class App extends Component<Props> {
+
+
+  // orientation
+
+  componentDidMount() {
+    Orientation.addOrientationListener(this._updateOrientation);
+    Orientation.addSpecificOrientationListener(this._updateSpecificOrientation);
+  }
+
+  componentWillUnmount() {
+    Orientation.removeOrientationListener(this._updateOrientation);
+    Orientation.removeSpecificOrientationListener(this._updateSpecificOrientation);
+  }
+
+  _updateOrientation = (orientation) => this.setState({ orientation });
+  _updateSpecificOrientation = (specificOrientation) => this.setState({ specificOrientation });
+  // end config orientation
+
+
   render() {
     return (
       <View style={{flex: 1}}>
         <VideoPlayer
           source={{ uri: 'https://vjs.zencdn.net/v/oceans.mp4' }}
+          controlTimeout={10000}
           navigator={this.props.navigator}
+          onEnterFullscreen={Orientation.lockToLandscape}
+          onExitFullscreen={Orientation.lockToPortrait}
         />
       </View>
     );
